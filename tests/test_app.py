e103dc5f1bfd2000be8dd06fde6e2a30409b0dfb
Malik-444 <malikclark08@gmail.com>
2026-08-21 21:55:53 -0400

    push from local

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+from app.app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+
+def test_metrics():
+    client = app.test_client()
+
+    response = client.get("/metrics")
+
+    assert response.status_code == 200
+    assert b"app_requests_total" in response.data
